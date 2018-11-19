8b342a9b4be33ed447f16a9e3362171196aa6b43
Robsonsjre <robsonsjre@gmail.com>
2018-11-19 09:32:56 +0800

    added new formspree email

--- scripts/index.js
+++ scripts/index.js
@@ -153,7 +153,7 @@ $(function() {
     e.preventDefault();
 
       $.ajax({
-          url: "https://formspree.io/mattwilliams85@gmail.com",
+          url: "https://formspree.io/robsonsjre@gmail.com",
           method: "POST",
           data: { message: $('form').serialize() },
           dataType: "json"
